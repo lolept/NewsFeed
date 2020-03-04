3d4f850bf9279c9a2b102135063bd266a2bf9cd0
lolept <lolept230@gmail.com>
2020-03-04 16:54:02 +0300

    Недомастаб картинки

--- app/src/main/java/com/example/gson/PostPage.java
+++ app/src/main/java/com/example/gson/PostPage.java
@@ -1,7 +1,12 @@
 package com.example.gson;
 
 import android.content.Intent;
+import android.graphics.Matrix;
+import android.graphics.PointF;
 import android.os.Bundle;
+import android.util.FloatMath;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,9 +17,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.picasso.Picasso;
 
-public class PostPage extends AppCompatActivity {
+public class PostPage extends AppCompatActivity implements View.OnTouchListener {
     TextView title,text, likes, author, comments;
     ImageView image;
+
+    private static final String TAG = "Touch" ;
+
+
+    Matrix matrix = new Matrix();
+    Matrix savedMatrix = new Matrix();
+    PointF start = new  PointF();
+    public static PointF mid = new PointF();
+
+
+    public static final int NONE = 0;
+    public static final int DRAG = 1;
+    public static final int ZOOM = 2;
+    public static int mode = NONE;
+
+    float oldDist;
+
+    private float[] matrixValues = new float[9];
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,5 +71,82 @@ public class PostPage extends AppCompatActivity {
                 finish();
             }
         });
+
+        image.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        ImageView view = (ImageView) v;
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+            case MotionEvent.ACTION_DOWN:
+
+                savedMatrix.set(matrix);
+                start.set(event.getX(), event.getY());
+                Log.d(TAG, "mode=DRAG" );
+                mode = DRAG;
+                break;
+
+            case MotionEvent.ACTION_POINTER_DOWN:
+
+                oldDist = spacing(event);
+                Log.d(TAG, "oldDist=" + oldDist);
+                if (oldDist > 10f) {
+
+                    savedMatrix.set(matrix);
+                    midPoint(mid, event);
+                    mode = ZOOM;
+                    Log.d(TAG, "mode=ZOOM" );
+                }
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+
+                if (mode == DRAG) {
+
+                    matrix.set(savedMatrix);
+                    matrix.postTranslate(event.getX() - start.x, event.getY() - start.y);
+                }
+                else if (mode == ZOOM) {
+
+                    float newDist = spacing(event);
+                    Log.d(TAG, "newDist=" + newDist);
+                    if (newDist > 10f) {
+
+                        matrix.set(savedMatrix);
+                        float scale = newDist / oldDist;
+                        matrix.postScale(scale, scale, mid.x, mid.y);
+                    }
+                }
+                break;
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_POINTER_UP:
+
+                mode = NONE;
+                Log.d(TAG, "mode=NONE" );
+                break;
+        }
+
+
+        view.setImageMatrix(matrix);
+
+        return true;
+    }
+
+    private float spacing(MotionEvent event) {
+        float x = event.getX(0) - event.getX(1);
+        float y = event.getY(0) - event.getY(1);
+        return (float)Math.sqrt(x * x + y * y);
+    }
+
+    private void midPoint(PointF point, MotionEvent event) {
+
+        float x = event.getX(0) + event.getX(1);
+        float y = event.getY(0) + event.getY(1);
+        point.set(x / 2, y / 2);
     }
 }
